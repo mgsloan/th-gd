d167709bed513e4b7062e1e0b1dacf98f5a6375a
Michael Sloan <mgsloan@gmail.com>
2012-05-06 17:08:08 -0700

    Updated to latest version of generic-deriving

--- Language/Haskell/TH/Generics.hs
+++ Language/Haskell/TH/Generics.hs
@@ -1,33 +1,42 @@
 {-# LANGUAGE TemplateHaskell, TypeFamilies, EmptyDataDecls #-}
 
 module Language.Haskell.TH.Generics
-  (module Generics.Deriving, module Generics.Deriving.TH) where
+  (module D) where
+
+import qualified Generics.Deriving as D
+
+import Generics.Deriving.TH (deriveMeta)
+import Control.Applicative  ((<$>))
 
 import Language.Haskell.TH.Syntax
-import Generics.Deriving.TH
-import Generics.Deriving
 
-deriveAll ''Exp
-deriveAll ''Match
-deriveAll ''Clause
-deriveAll ''Pat
-deriveAll ''Type
-deriveAll ''Dec
-deriveAll ''Name
-deriveAll ''FunDep
-deriveAll ''Pred
-deriveAll ''TyVarBndr
-deriveAll ''Kind
-deriveAll ''Con
-deriveAll ''Pragma
-deriveAll ''Foreign
-deriveAll ''FamFlavour
-deriveAll ''Range
-deriveAll ''Stmt
-deriveAll ''Body
-deriveAll ''ClassInstance
-deriveAll ''Info
-deriveAll ''Guard
-deriveAll ''(,)
-deriveAll ''(,,)
-deriveAll ''Strict
+$(concat <$> mapM deriveMeta
+  [ ''(,)
+  , ''(,,)
+  , ''Body
+  , ''Callconv
+  , ''Clause
+  , ''Con
+  , ''Dec
+  , ''Exp
+  , ''FamFlavour
+  , ''Fixity
+  , ''FixityDirection
+  , ''Foreign
+  , ''FunDep
+  , ''Guard
+  , ''Info
+  , ''InlineSpec
+  , ''Kind
+  , ''Lit
+  , ''Match
+  , ''Pat
+  , ''Pragma
+  , ''Pred
+  , ''Range
+  , ''Safety
+  , ''Stmt
+  , ''Strict
+  , ''Type
+  , ''TyVarBndr
+  ])
